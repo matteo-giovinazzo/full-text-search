1e38b06e570c700826ad3ced432cffc5ad12e69d
matteogiovi <matteogiovinazzo10@icloud.com>
2025-10-17 11:13:01 +0200

    add fuzzy search version with score string

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,88 @@
 //va fatto un parser del testo
 //dobbiamo categorizzare i singoli caratteri
 
+//VERSIONE 5
+//ora dobbiamo fare una ricerca parziale
+
+//la logica del fare pezzettini piccolini è sbagliata
+/*
+
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+fn split_query(my_query: String, n: usize) -> Vec<String> {
+    let chars: Vec<char> = my_query.to_lowercase().chars().collect();
+    //raccogliamo tutti i pezzetti attraverso una finestra mobile
+    chars
+        .windows(n)
+        .map(|finestra| finestra.iter().collect())
+        .collect()
+}
+
+//con questa funzione sopra abbiamo trasformato la nostra query in un vettore di stringhe
+//dove ci son tutti pezzettini di lunghezza n
+
+//creiamone una per calcolare l'indice di jaccard fra due hash set == i due contenitori dei pieces
+fn jaccard(set1: &HashSet<&str>, set2: &HashSet<&str>) -> f64 {
+    let intersection = set1.intersection(set2).count() as f64;
+    let union = (set1.len() + set2.len()) as f64 - intersection;
+    if union == 0.0 {
+        1.0
+    } else {
+        intersection / union
+    }
+}
+
+fn main() -> io::Result<()> {
+    let file_path = "testo.txt";
+    let n = 3;
+    let my_query = "imeless, an".to_string();
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+    let mut all_pieces = Vec::new();
+    for line_result in reader.lines() {
+        let row = line_result?;
+        let pieces_of_row = split_query(row, n);
+        all_pieces.extend(pieces_of_row);
+    }
+
+    //stampiamo i primi 100 pezzetti per vederli
+    for n_pieces in all_pieces.iter().take(100) {
+        println!("- '{}'", n_pieces);
+    }
+
+    let query_pieces = split_query(my_query, n);
+    let query_set: HashSet<&str> = query_pieces.iter().map(AsRef::as_ref).collect(); //così ora è una hashset
+
+    let mut best_match: Option<Vec<String>> = None;
+    let mut best_score = -1.0; //inizializziamo con punteggio imposibile
+
+    for window in all_pieces.windows(query_pieces.len()) {
+        let window_set: HashSet<&str> = window.iter().map(AsRef::as_ref).collect();
+        let score = jaccard(&query_set, &window_set);
+        if score > best_score {
+            best_score = score;
+            best_match = Some(window.to_vec());
+        }
+    }
+
+    if let Some(match_trovato) = best_match {
+        println!("La corrispondenza più simile trovata è:");
+        println!("- Sequenza: {:?}", match_trovato);
+        println!("- Punteggio di similarità (Jaccard): {:.2}", best_score);
+    } else {
+        println!("Nessuna corrispondenza trovata.");
+    }
+
+    Ok(())
+}
+
+*/
+
+//nuova strategia versione 5
+//riutilizziamo tokenize della versione 4
+
 use clap::Parser;
 use std::error::Error;
 use std::fs::File;
@@ -41,22 +123,24 @@ enum TokenType {
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    ///cerca la parola o frase o collezione di caratteri
+    ///what do you want to search in text?
     query: String,
 
-    ///il file nella quale cercare si chiama
+    ///in which text file do you want to search?
     file_path: String,
 
-    ///decidi se ricerca case sensitive (true) o case insensitive (false)
+    ///choose the min score
+    threshold: f64,
+
+    ///decide if you want case-sensitive search (true) or not case sensitive search (false, default)
     #[arg(short, long)]
     case_sensitive: bool,
 
-    /// Ignora la punteggiatura e i caratteri speciali nella ricerca (true) o mantieni tutto (false)
-    #[arg(short = 's', long)]
-    ignore_special: bool,
+    ///decide if you want to include special character (true) or exclude (false, default)
+    #[arg(long, default_value_t = false)]
+    include_special: bool,
 }
 
-//funzione di suporto per scrivere il testo
 impl TokenType {
     fn as_str(&self) -> String {
         match self {
@@ -111,14 +195,22 @@ fn tokenize(testo: &str, case_sensitive: bool) -> Vec<TokenType> {
     tokens
 }
 
+fn transform_in_string(tokens: &[TokenType]) -> String {
+    tokens
+        .iter()
+        .map(|t| t.as_str())
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
 fn find_best_token_sequence(
     all_tokens: &[TokenType],
     query_tokens: &[TokenType],
     threshold: f64,
-    ignore_special: bool,
-) -> Option<Vec<TokenType>> {
-    //un controllo "obbligatorio"
-    let (all_tokens_to_search, query_tokens_to_search) = if ignore_special {
+    include_special: bool,
+    original_query: &str,
+) -> Vec<(Vec<TokenType>, f64)> {
+    let (all_tokens_to_search, query_tokens_to_search) = if !include_special {
         let filtered_all = all_tokens
             .iter()
             .filter(|t| matches!(t, TokenType::Word(_)))
@@ -137,43 +229,50 @@ fn find_best_token_sequence(
     if query_tokens_to_search.is_empty()
         || all_tokens_to_search.len() < query_tokens_to_search.len()
     {
-        return None;
+        return Vec::new();
     }
 
-    let mut best_match: Option<&[TokenType]> = None;
-    let mut best_score = -1.0; //assegno punteggio impossibile
+    let mut first_match = Vec::new();
 
     for window in all_tokens_to_search.windows(query_tokens_to_search.len()) {
         let score: f64 = query_tokens_to_search
             .iter()
             .zip(window.iter())
             .map(|(query_tok, text_tok)| {
-                strsim::normalized_damerau_levenshtein(&query_tok.as_str(), &text_tok.as_str())
+                let query_str = query_tok.as_str();
+                if let TokenType::Word(_) = query_tok {
+                    if query_str.len() <= 3 {
+                        if query_tok == text_tok { 1.0 } else { 0.0 }
+                    } else {
+                        strsim::normalized_damerau_levenshtein(&query_str, &text_tok.as_str())
+                    }
+                } else if query_tok == text_tok {
+                    1.0
+                } else {
+                    0.0
+                }
             })
             .sum::<f64>()
-            / query_tokens.len() as f64;
+            / query_tokens_to_search.len() as f64;
 
-        if score > best_score {
-            best_score = score;
-            best_match = Some(window);
+        if score >= threshold {
+            first_match.push(window.to_vec());
         }
     }
-
-    if best_score >= threshold {
-        best_match.map(|slice| slice.to_vec())
-    } else {
-        None
+    let mut results_with_score = Vec::new();
+    for sequence in first_match {
+        let match_string = transform_in_string(&sequence);
+        let final_score = strsim::normalized_damerau_levenshtein(original_query, &match_string);
+        results_with_score.push((sequence, final_score));
     }
+    results_with_score
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
-    // Unisce tutti gli argomenti in un'unica frase di ricerca
     let query_phrase = args.query;
-    let threshold: f64 = 0.8;
     let file_path = args.file_path;
 
-    // Tokenizza sia la query che il file
     let query_tokens = tokenize(&query_phrase, args.case_sensitive);
 
     let file = File::open(file_path)?;
@@ -184,16 +283,24 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     println!(
-        "Cerco la sequenza {:?} con soglia >= {}...",
-        query_tokens, threshold
+        "i search {:?} with a score  >= {}...",
+        query_tokens, args.threshold
     );
 
-    match find_best_token_sequence(&all_tokens, &query_tokens, threshold, args.ignore_special) {
-        Some(found_sequence) => {
-            println!(r"\Trovata la migliore corrispondenza: {:?}", found_sequence);
-        }
-        None => {
-            println!("Nessuna corrispondenza sufficientemente simile trovata.");
+    let results = find_best_token_sequence(
+        &all_tokens,
+        &query_tokens,
+        args.threshold,
+        args.include_special,
+        &query_phrase,
+    );
+
+    if results.is_empty() {
+        println!("no correspondences enough similar ");
+    } else {
+        println!("find {} correspondences :", results.len());
+        for found_sequence in results {
+            println!("- {:?}", found_sequence);
         }
     }
 
